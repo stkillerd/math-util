10fe5c1da5ec1aa1d155535dd866f719b5fcc9e4
stkillerd <hoavqse130608@fpt.edu.vn>
2020-11-17 15:50:31 +0700

    add JUnit library

---
+++ test/hoa/util/test/MathUtilityTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoa.util.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author qhoav
+ */
+public class MathUtilityTest {
+    
+    public MathUtilityTest() {
+    }
+    
+}
