bef11e97f3d1d81fcb9ebe2d16edf2a959cc0935
stkillerd <hoavqse130608@fpt.edu.vn>
2020-11-19 13:56:35 +0700

    failed

--- test/hoa/util/test/MathUtilityTest.java
+++ test/hoa/util/test/MathUtilityTest.java
@@ -5,6 +5,8 @@
  */
 package hoa.util.test;
 
+import hoa.util.MathUtility;
+import math.util.MathUtil;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,8 +15,19 @@ import static org.junit.Assert.*;
  * @author qhoav
  */
 public class MathUtilityTest {
-    
-    public MathUtilityTest() {
+
+    @Test // kí hiệu chơi với JUnit đưa ra, biến cái hàm thành hàm main
+    //public static void manin(), app có nhiều main
+    public void getFactorial_RunsWell_IfValidArgument() {
+            assertEquals(120, MathUtility.getFactorial(5));
+            assertEquals(1, MathUtility.getFactorial(0));
+            assertEquals(720, MathUtility.getFactorial(6));
+            assertEquals(1, MathUtility.getFactorial(1));
+            assertEquals(12, MathUtility.getFactorial(5));
     }
-    
+    // viết code để test code 
+    // viết code dùng thư viện JUnit,xUnit, Jasmine để test
+    //xem code chính chạy ổn không trc khi đem cho khách hàng/QC
+    //nếu màu đỏ k cho clean and build 
+    //TDD viết code kèm với viết code test test driven development
 }
